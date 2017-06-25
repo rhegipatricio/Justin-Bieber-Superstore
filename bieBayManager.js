2b6afddd62376e03db9090c20bc3d3fa7460c15b
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-25 16:33:40 -0400

    wrote error log and inquirer prompt

--- bieBayManager.js
+++ bieBayManager.js
@@ -17,3 +17,17 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
 	if (err) throw err;
+
+	console.log('     ADMIN LOGIN\n');
+	initialPrompt();
+
+	function initialPrompt () {
+		connection.query('SELECT `item_id`, `product_name`, `price` FROM `products`', function (err, data) {
+			if (err) throw err;
+			for (var i = 0; i < data.length; i++) {
+				itemList.push(data[i]);
+				console.log('id', itemsList[i].items_id + ':', itemsList[i].product_name, '$' + itemList[i].price);
+			}
+			inquirer.prompt([
+
+	}
