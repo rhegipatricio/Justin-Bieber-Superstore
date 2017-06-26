b70176018e9c0662f548da7fc5b673ce75e5e2c1
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-26 02:44:38 -0400

    commented out code explaining what each function does

--- bieBayCustomer.js
+++ bieBayCustomer.js
@@ -1,12 +1,15 @@
+//required to call for mysql and inquirer
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+//additional variables and itemList array
 var itemList = [];
 var idChosen;
 var quantityChosen;
 var total;
 var changeStock;
 
+//used to connect to the biebay database in mysql
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
@@ -15,12 +18,14 @@ var connection = mysql.createConnection({
 	database: "biebay"
 })
 
+//logs errors and greeting from Bieber that the user sees when app is run
 connection.connect(function(err) {
 	if (err) throw err;
 	console.log('');
 	console.log("Bieber says What's Good?! Thank you for always Beliebing!\nCheck out my products on sale: ");
 	console.log('');
 	viewItems();
+	//items listed for sale
 	function viewItems() {
 		connection.query("SELECT `item_id`, `product_name`, `price` FROM `products`", function(err, data) {
 			if (err) throw err;
@@ -28,12 +33,14 @@ connection.connect(function(err) {
 				itemList.push(data[i]);
 				console.log("id", itemList[i].item_id + ":", itemList[i].product_name, "$" + itemList[i].price);
 		}
+		//inquirer prompt for user to choose item
 		inquirer.prompt([
 			{
 				type: 'input',
 				message: 'What product would you want today? (choose by item id number)',
 				name: 'itemChoice'
 			}
+		//Depending on user choice, it'll show either error or what user chose	
 		]).then(function (response) {
 			idChosen = response.itemChoice;
 			connection.query("SELECT `item_id`, `product_name`, `price`, `stock_quantity` FROM `products` WHERE `item_id` = ?", [idChosen], function(err, data) {
@@ -45,7 +52,7 @@ connection.connect(function(err) {
 						checkAmount();
 					}
 
-				
+				//asks user for quantity on selected item
 				function checkAmount () {
 					inquirer.prompt ([
 						{
@@ -53,6 +60,7 @@ connection.connect(function(err) {
 							message: 'What is the number of ' + data[0].product_name + '\'s you want to buy?',
 							name: 'quantity'
 						}
+					//final statement on how much is due or message for insufficient stock and to choose different amount	
 					]).then(function (response) {
 						quantityChosen = response.quantity;
 						 if (data[0].stock_quantity > quantityChosen) {
